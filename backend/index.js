4170b419abb72faff0c5744e87ef3a336eebff19
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-10-13 18:52:58 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,7 @@ app.use((req, res, next) => {
   );
   next();
 })
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json())
 
 
 // error handler
