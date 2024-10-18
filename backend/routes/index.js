543985cf3f49033a1abe3e710de4e7d73d7fa4a6
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-10-18 16:23:08 +0500

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express =require('express')
 const routes = express.Router()
 
 
-// routes.use(require('./authRutes'))
+routes.use(require('./authRutes'))
 // routes.use(require('./foodRoutes'))
 // routes.use(require('./orderRoutes'))
 // routes.use(require('./cartRoutes'))
