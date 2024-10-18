c9b8acb36d22e2566e27a4fa8873fa1d07fa64c4
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-10-18 16:58:06 +0500

    Update foodController.js

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -32,51 +32,51 @@ const addFoodByAdmin = async (req, res, next) => {
 }
 const addFood = async (req, res, next) => {
 
-    try {
-        const foodData = req.body;
-        const file_name = req.file.filename
+    // try {
+    //     const foodData = req.body;
+    //     const file_name = req.file.filename
 
-        const { name, description, price, category, userId } = foodData;
-        console.log('price', JSON.parse(req.body.price))
+    //     const { name, description, price, category, userId } = foodData;
+    //     console.log('price', JSON.parse(req.body.price))
 
-        const product = new Food({
-            name,
-            description,
-            image: file_name,
-            price:JSON.parse(req.body.price),
-            category,
-            user: userId
-        });
-        await product.save();
-        res.json({
-            success: true,
-            message: "Food Added successfully"
-        })
+    //     const product = new Food({
+    //         name,
+    //         description,
+    //         image: file_name,
+    //         price:JSON.parse(req.body.price),
+    //         category,
+    //         user: userId
+    //     });
+    //     await product.save();
+    //     res.json({
+    //         success: true,
+    //         message: "Food Added successfully"
+    //     })
 
-    } catch (err) {
-        res.json({
-            success: false,
-            message: "Error",
-            error: err
-        })
-    }
+    // } catch (err) {
+    //     res.json({
+    //         success: false,
+    //         message: "Error",
+    //         error: err
+    //     })
+    // }
 
 }
 const addCategory = async (req, res, next) => {
-    try {
-        const categoryData = req.body;
+    // try {
+    //     const categoryData = req.body;
  
-        const { name } = categoryData;
-        const category = new foodCategory({
-            name,
-        });
-        await category.save();
-        return res
-            .status(201)
-            .json({ message: "Products Category added successfully" });
-    } catch (err) {
-        next(err);
-    }
+    //     const { name } = categoryData;
+    //     const category = new foodCategory({
+    //         name,
+    //     });
+    //     await category.save();
+    //     return res
+    //         .status(201)
+    //         .json({ message: "Products Category added successfully" });
+    // } catch (err) {
+    //     next(err);
+    // }
 }
 const getCategory = async (req, res, next) => {
     try {
@@ -102,123 +102,123 @@ const getAllItems = async (req, res, next) => {
 }
 
 const removeItem = async (req, res, next) => {
-    try {
-        let item = await Food.findById(req.body.id);
-        console.log(item, 'item ===>')
-        await Food.findByIdAndDelete(req.body.id)
-        return res.status(201).json({ success: true, message: 'Food Removed' });
-    } catch (err) {
-        next(err);
-    }
+    // try {
+    //     let item = await Food.findById(req.body.id);
+    //     console.log(item, 'item ===>')
+    //     await Food.findByIdAndDelete(req.body.id)
+    //     return res.status(201).json({ success: true, message: 'Food Removed' });
+    // } catch (err) {
+    //     next(err);
+    // }
 }
 const getFoodById = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        if (!mongoose.isValidObjectId(id)) {
-            return next(createError(400, "Invalid product ID"));
-        }
-        const food = await Food.findById(id).populate('user');
-        if (!food) {
-            return next(createError(404, "Food not found"));
-        }
-        return res.status(200).json(food);
-    } catch (err) {
-        next(err);
-    }
+    // try {
+    //     const { id } = req.params;
+    //     if (!mongoose.isValidObjectId(id)) {
+    //         return next(createError(400, "Invalid product ID"));
+    //     }
+    //     const food = await Food.findById(id).populate('user');
+    //     if (!food) {
+    //         return next(createError(404, "Food not found"));
+    //     }
+    //     return res.status(200).json(food);
+    // } catch (err) {
+    //     next(err);
+    // }
 }
 
 
 const placeOrder = async (req, res, next) => {
-    try {
-        const { products, address, totalAmount } = req.body;
-        const userJWTId = req.user;
-        const user = await User.findById(userJWTId);
+    // try {
+    //     const { products, address, totalAmount } = req.body;
+    //     const userJWTId = req.user;
+    //     const user = await User.findById(userJWTId);
 
-        const order = new Orders({
-            products,
-            user: user._id,
-            total_amount: totalAmount,
-            address,
-        });
+    //     const order = new Orders({
+    //         products,
+    //         user: user._id,
+    //         total_amount: totalAmount,
+    //         address,
+    //     });
 
-        await order.save();
-        user.cart = [];
-        await user.save();
-        return res
-            .status(200)
-            .json({ message: "Order placed successfully", order });
-    } catch (err) {
-        next(err);
-    }
+    //     await order.save();
+    //     user.cart = [];
+    //     await user.save();
+    //     return res
+    //         .status(200)
+    //         .json({ message: "Order placed successfully", order });
+    // } catch (err) {
+    //     next(err);
+    // }
 };
 
 const getAllOrders = async (req, res, next) => {
-    try {
-        const { productId } = req.body;
-        const userJWTId = req.user;
-        const user = await User.findById(userJWTId);
-        if (!user.favourites.includes(productId)) {
-            user.favourites.push(productId);
-            await user.save();
-        }
-        return res
-            .status(200)
-            .json({ message: "Product added to favorites successfully", user });
-    } catch (err) {
-        next(err);
-    }
+    // try {
+    //     const { productId } = req.body;
+    //     const userJWTId = req.user;
+    //     const user = await User.findById(userJWTId);
+    //     if (!user.favourites.includes(productId)) {
+    //         user.favourites.push(productId);
+    //         await user.save();
+    //     }
+    //     return res
+    //         .status(200)
+    //         .json({ message: "Product added to favorites successfully", user });
+    // } catch (err) {
+    //     next(err);
+    // }
 };
 
-//Favorites
+// Favorites
 
-// const removeFromFavorites = async (req, res, next) => {
-//     try {
-//         const { productId } = req.body;
-//         const userJWT = req.user;
-//         const user = await User.findById(userJWT.id);
-//         user.favourites = user.favourites.filter((fav) => !fav.equals(productId));
-//         await user.save();
+const removeFromFavorites = async (req, res, next) => {
+    // try {
+    //     const { productId } = req.body;
+    //     const userJWT = req.user;
+    //     const user = await User.findById(userJWT.id);
+    //     user.favourites = user.favourites.filter((fav) => !fav.equals(productId));
+    //     await user.save();
 
-//         return res
-//             .status(200)
-//             .json({ message: "Product removed from favorites successfully", user });
-//     } catch (err) {
-//         next(err);
-//     }
-// };
+    //     return res
+    //         .status(200)
+    //         .json({ message: "Product removed from favorites successfully", user });
+    // } catch (err) {
+    //     next(err);
+    // }
+};
 
-// const addToFavorites = async (req, res, next) => {
-//     try {
-//         const { productId } = req.body;
-//         const userJWT = req.user;
-//         const user = await User.findById(userJWT.id);
+const addToFavorites = async (req, res, next) => {
+    // try {
+    //     const { productId } = req.body;
+    //     const userJWT = req.user;
+    //     const user = await User.findById(userJWT.id);
 
-//         if (!user.favourites.includes(productId)) {
-//             user.favourites.push(productId);
-//             await user.save();
-//         }
+    //     if (!user.favourites.includes(productId)) {
+    //         user.favourites.push(productId);
+    //         await user.save();
+    //     }
 
-//         return res
-//             .status(200)
-//             .json({ message: "Product added to favorites successfully", user });
-//     } catch (err) {
-//         next(err);
-//     }
-// };
+    //     return res
+    //         .status(200)
+    //         .json({ message: "Product added to favorites successfully", user });
+    // } catch (err) {
+    //     next(err);
+    // }
+};
 
-// const getUserFavorites = async (req, res, next) => {
-//     try {
-//         const userId = req.user.id;
-//         const user = await User.findById(userId).populate("favourites").exec();
-//         if (!user) {
-//             return next(createError(404, "User not found"));
-//         }
-//         const favoriteProducts = user.favourites;
-//         return res.status(200).json(favoriteProducts);
-//     } catch (err) {
-//         next(err);
-//     }
-// };
+const getUserFavorites = async (req, res, next) => {
+    // try {
+    //     const userId = req.user.id;
+    //     const user = await User.findById(userId).populate("favourites").exec();
+    //     if (!user) {
+    //         return next(createError(404, "User not found"));
+    //     }
+    //     const favoriteProducts = user.favourites;
+    //     return res.status(200).json(favoriteProducts);
+    // } catch (err) {
+    //     next(err);
+    // }
+};
 module.exports = {
     addFood, addFoodByAdmin, getAllItems, getFoodById,
     removeFromFavorites,
