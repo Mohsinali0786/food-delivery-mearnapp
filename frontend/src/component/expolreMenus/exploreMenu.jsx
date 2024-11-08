72bfa01f3092359308ec939fcfbceb17950f6077
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-11-08 18:45:32 +0500

    update exploreMenu

--- frontend/src/component/expolreMenus/exploreMenu.jsx
+++ frontend/src/component/expolreMenus/exploreMenu.jsx
@@ -1,12 +1,14 @@
 import { useContext, useEffect, useState } from "react";
-import { getAllFoods } from "../../utils/commonMethods";
-import { getRequest } from "../../utils/service";
+import { getAllFoods, getFoodCategory } from "../../utils/commonMethods";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { getRequest ,deleteRequest } from "../../utils/service";
 import './exploreMenu.css'
 import sampleImg from "../../assets/food.jpg"
 import { StoreContext } from "../../context/storeContext";
 export default function ExploreMenu({category,setCategory}) {
   const [menuList, setMenuList] = useState([]);
-  const {allCategory} = useContext(StoreContext)
+  const {allCategory,setAllCategory} = useContext(StoreContext)
   useEffect(() => {
     getAllFoods();
   }, []);
@@ -14,7 +16,14 @@ export default function ExploreMenu({category,setCategory}) {
     const allFood = await getRequest("/getAllItems");
     setMenuList(allFood);
   };
-  console.log("allll fooodds", category);
+  const deleteCategory = async (id) => {
+    // setLoading(true);
+     await deleteRequest(`/remove-category/${id}`);
+    let res = await getFoodCategory()
+    console.log('getFoodCategory()',res)
+     setAllCategory(res)
+   
+  };
   return (
     <div className="exploreMenu" id="exploreMenu">
       <h1>Explore Our Menu</h1>
@@ -26,7 +35,8 @@ export default function ExploreMenu({category,setCategory}) {
         {allCategory?.map((item, index) => {
           return (
             <div onClick={()=>setCategory((prev)=>prev==item?.name ? 'All':item?.name)} key={index} className="explore-menu-item-list">
-              <img className={category == item?.name ? "active" : ""} src={sampleImg}/>
+             <CancelIcon className={category == item?.name ? "removeIconForActive" : 'removeIcon'} onClick={()=>{deleteCategory(item?._id)}}/>
+             <img className={category == item?.name ? "active" : ""} src={sampleImg}/>
               <p>{item.name}</p>
             </div>
           );
