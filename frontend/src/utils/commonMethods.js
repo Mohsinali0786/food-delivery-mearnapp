856247571a475738d9daaf4053fa922e1064e03f
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-11-08 18:36:28 +0500

    added getFoodCategory

--- frontend/src/utils/commonMethods.js
+++ frontend/src/utils/commonMethods.js
@@ -3,3 +3,8 @@ export const getAllFoods = async (url) => {
     const allFood = await getRequest("/getAllItems");
     return allFood
 }
+export const getFoodCategory = async () => {
+    const data = await getRequest(`/get-category`);
+    // setCategory(data?.data?.allCategories);
+    return data?.allCategories
+  }
