75ee221c60a529e0f126603beea6d004685fc1cb
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-11-08 18:45:04 +0500

    updateFoodItem

--- frontend/src/component/foodItem/foodItem.jsx
+++ frontend/src/component/foodItem/foodItem.jsx
@@ -5,36 +5,42 @@ import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { useContext, useState } from "react";
 import { StoreContext } from "../../context/storeContext";
-const FoodItem = ({ key, id, name, image, price ,  description ,quantity }) => {
+const FoodItem = ({ key, id, name, image, price, description, quantity }) => {
   const [itemCount, setItemCount] = useState(0);
   const { cartItems, addToCart, removeFromCart, url } =
     useContext(StoreContext);
-    console.log(image,'image',id)
-    console.log(!cartItems,'!cartItem[id]')
+  console.log(image, "image", id);
+  console.log(!cartItems, "!cartItem[id]");
 
   return (
     <div className="food-item">
       <div className="food-item-image-container">
         {/* <img src={`${url}/images/${image}`} alt="" className="food-item-img" /> */}
         <img src={image} alt="" className="food-item-img" />
-        {cartItems && !cartItems[id] ? (
-          <AddIcon onClick={() => addToCart(id)} className="add" />
-        ) : (
-          <div className="food-item-counter">
-            <AddCircleOutlineIcon
-              onClick={() => addToCart(id , quantity)}
-              className="addItemsIcon"
-              sx={{ backgroundColor: "green" }}
-            />
-            <p>{cartItems[id]}</p>
-            <RemoveCircleOutlineIcon
-              onClick={() => removeFromCart(id)}
-              sx={{ backgroundColor: "red" }}
-              className="addItemsIcon"
-            />
-          </div>
-        )}
+        {
+          quantity > 0 ? 
+          cartItems && !cartItems[id] ? (
+            <AddIcon onClick={() => addToCart(id)} className="add" />
+          ) : (
+            <div className="food-item-counter">
+              <AddCircleOutlineIcon
+                onClick={() => addToCart(id, quantity)}
+                className="addItemsIcon"
+                sx={{ backgroundColor: "green" }}
+              />
+              <p>{cartItems[id]}</p>
+              <RemoveCircleOutlineIcon
+                onClick={() => removeFromCart(id)}
+                sx={{ backgroundColor: "red" }}
+                className="addItemsIcon"
+              />
+            </div>
+          )
+          :
+          null
+        }
         <div className="food-item-info">
+            { quantity < 1 ? <p className="outOfStock">Out of Stock</p> : null}
           <div className="food-item-name-rating">
             <p>{name}</p>
             {/* <img src="" alt="" /> */}
@@ -44,7 +50,15 @@ const FoodItem = ({ key, id, name, image, price ,  description ,quantity }) => {
           <div className="d-flex justify-content-between">
             <div>
               {price?.org ? (
-                <p className={price?.mrp < 1 || price?.mrp == price?.org ? 'food-item-price item-price-margin' : 'line-through'}>Price Rs {price?.org}</p>
+                <p
+                  className={
+                    price?.mrp < 1 || price?.mrp == price?.org
+                      ? "food-item-price item-price-margin"
+                      : "line-through"
+                  }
+                >
+                  Price Rs {price?.org}
+                </p>
               ) : null}
               {price?.mrp && price?.mrp != price?.org ? (
                 <p className="">
@@ -60,8 +74,9 @@ const FoodItem = ({ key, id, name, image, price ,  description ,quantity }) => {
               </p>
             ) : null}
           </div>
-          <p className="mp-0">Quantity:<b>{quantity}</b></p>
-
+          <p className="mp-0">
+            Quantity:<b>{quantity}</b>
+          </p>
         </div>
       </div>
     </div>
