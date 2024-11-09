77befd58aaa6d0c971bd9a56692ff42d90ed5346
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-11-09 13:42:26 +0500

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const orderModels = require('../models/orderModels');
 require('dotenv').config();
 
 // console.log(`process.env.STRIPE_SECRETE_KEY  ${process.env.STRIPE_SECRETE_KEY}`)
-const stripe = new Stripe('sk_test_51Q14u6RxWi5FKjb0OpPaTMEuNL9cx8adEqhKdvZ32QTWMkerNRbKhHSLpJ2VIDoaa6Hg3bK8o9hMVtmnArEPk1gy00JOBT3jhV')
+const stripe = new Stripe(process.env.STRIPE_SECRETE_KEY)
 
 const frontend_Url = 'https://food-delivery-f-mearnapp.vercel.app/'
 const placeOrder = async (req, res, next) => {
