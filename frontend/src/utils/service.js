2a7cecb26f71bdb2b608b145ff318654f89a32e4
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-11-22 12:48:39 +0500

    Update service.js

--- frontend/src/utils/service.js
+++ frontend/src/utils/service.js
@@ -1,15 +1,17 @@
 // Neer used Only sample 
 
 import axios from 'axios'
-// export const baseUrl = "http://localhost:5001/api";
-export const baseUrl = "https://food-delivery-b-mearnapp.vercel.app/api";
+export const baseUrl = "http://localhost:5001/api";
+// export const baseUrl = "https://food-delivery-b-mearnapp.vercel.app/api";
 
 
 export const postRequest = async (url, body , token) => {
+  console.log(token,'token')
   const response = await fetch(baseUrl+url, {
     method: "POST",
     headers: {
-      'Authorization': `Bearer ${token}`,
+      // 'Authorization': `Bearer ${token}`,
+      'token':token,
       "Content-Type": "application/json",
     },
     body:JSON.stringify(body),
@@ -48,9 +50,14 @@ export const postRequest = async (url, body , token) => {
 
 //     return data;
 //   };
-export const getRequest = async (url) => {
+export const getRequest = async (url,token) => {
   let fullUrl = baseUrl + url
-  const response = await axios.get(fullUrl)
+  let config = {
+    headers: {
+      'token':token
+    }
+  }
+  const response = await axios.get(fullUrl,config)
     .then(response => {
       console.log(response,'get Request');
 
@@ -65,9 +72,15 @@ export const getRequest = async (url) => {
     return response;
 }
 
-export const patchRequest = async (url,body) => {
+export const patchRequest = async (url,body,token) => {
+  const config = {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
   let fullUrl = baseUrl + url
-  const response = await axios.post(fullUrl,body)
+  const response = await axios.post(fullUrl,body,config)
     .then(response => {
       console.log(response,'response');
 
