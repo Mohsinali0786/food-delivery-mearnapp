110d3ba575006f6d6f3da734b853ba94f5fb4379
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-11-29 16:57:02 +0500

    Update userFavourite.jsx

--- frontend/src/pages/userFavourite/userFavourite.jsx
+++ frontend/src/pages/userFavourite/userFavourite.jsx
@@ -5,50 +5,57 @@ import { Button } from "@mui/material";
 import noData from "../../assets/noData.png";
 import "./userFavourite.css";
 import axios from "axios";
-import { FoodItem } from "../../component";
+import { FoodItem, Spinner } from "../../component";
 import { getRequest } from "../../utils/service";
 export default function MyFaourites() {
-  const { loginData ,url ,token} = useContext(StoreContext);
+  const { loginData, url, token } = useContext(StoreContext);
   const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(false);
 
   useEffect(() => {
-    const loginInfo = JSON.parse(localStorage.getItem("loginInfo"))
-    if(loginInfo?._id){
-      getFavourites()
+    const loginInfo = JSON.parse(localStorage.getItem("loginInfo"));
+    if (loginInfo?._id) {
+      getFavourites();
     }
-},[]);
-const getFavourites = async ()=>{
-    let favouriteData = await getRequest('/favorite',localStorage.getItem("token"))
+  }, []);
+  const getFavourites = async () => {
+    setLoader(true);
+    let favouriteData = await getRequest(
+      "/favorite",
+      localStorage.getItem("token")
+    );
     // console.log(favouriteData.data,'favouriteData')
-    if(favouriteData) setData(favouriteData)
-  }
-  console.log('data',data)
+    if (favouriteData) setData(favouriteData);
+    setLoader(false);
+  };
+  console.log("data", data);
   return (
     <div className="my-orders">
-      {data && data.length > 0 ? (
+      {loader ? (
+        <Spinner />
+      ) : data && data.length > 0 ? (
         <div className="food-display-list">
           {data &&
             data.map((item, index) => {
               console.log(item, " ============> data");
-            //   if (category == "All" || category == item.category || !category) {
-                return (
-                  <FoodItem
-                    quantity={item.quantity}
-                    description={item?.description}
-                    key={index}
-                    id={item?._id}
-                    name={item?.name}
-                    image={item?.image?.url}
-                    price={item?.price}
-                    category={item?.category}
-                  />
-                );
-            //   }
+              //   if (category == "All" || category == item.category || !category) {
+              return (
+                <FoodItem
+                  quantity={item.quantity}
+                  description={item?.description}
+                  key={index}
+                  id={item?._id}
+                  name={item?.name}
+                  image={item?.image?.url}
+                  price={item?.price}
+                  category={item?.category}
+                />
+              );
+              //   }
             })}
         </div>
       ) : (
-        <h3 style={{ textAlign: "center" }}>
-        </h3>
+        <h3 style={{ textAlign: "center" }}>No Favourite Found against</h3>
       )}
     </div>
   );
